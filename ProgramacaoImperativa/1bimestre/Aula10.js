b0135104fa6f82027e8f7261cdf3a7c28996985f
Rodrigo-Valle <rodrigocostavalle@gmail.com>
2021-11-15 00:09:32 -0300

    Entrega I Checkpoint frontend

---
+++ ProgramacaoImperativa/1bimestre/Aula10.js
@@ -0,0 +1,42 @@
+let numeros = [10, 15, 20, 18, 16, 14];
+
+console.log(numeros[5]);
+numeros[3] = 19;
+numeros.push(12);
+numeros.shift(23);
+let numeros2 = [numeros.pop()];
+let comp = numeros[0] == numeros2[0];
+numeros2.push(comp);
+
+console.log(numeros);
+console.log(numeros2);
+
+let numbers =[22, 33, 54, 66, 72];
+console.log(numbers[numbers.length - 1]);
+
+//let grupoDeAmigos = [ ["Harry", "Ron", "Hermione"], ["Spiderman", "Hulk", "Ironman"], ["Penélope Glamour", "Pierre Nodoyuna","Patán"] ];
+//console.log(grupoDeAmigos[1][0]);
+
+let str = "una string qualquer";
+let grupoDeAmigos = [[45, 89, 0], ["Digital", "House", true],["string", "123","false", 54, true, str]];
+console.log(grupoDeAmigos[2][grupoDeAmigos[2].length - 1]);
+
+
+let numerosInversosFacil = numeros.reverse();
+console.log(numerosInversosFacil);
+
+console.log(mprimirInverso(numeros));
+
+function ImprimirInverso(array){
+    let novoArray = [];
+    let tamanho = array.lenght;
+    for (let index = 0; index < array.length; index++) {
+        novoArray.push(array[tamanho]);
+        tamanho = tamanho - 1;
+    }
+    return novoArray;
+
+}
+
+
+
