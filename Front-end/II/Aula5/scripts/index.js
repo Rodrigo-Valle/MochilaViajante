061f32646e3fcb5e0dd877fb78540adc66116f47
Rodrigo-Valle <rodrigocostavalle@gmail.com>
2022-05-17 20:39:56 -0300

    Aula1 OO

--- Front-end/II/Aula5/scripts/index.js
+++
@@ -1,8 +0,0 @@
-let body = document.querySelector('body');
-let h1 = document.querySelector('h1')
-
-body.style.backgroundColor = "#88e690";
-h1.innerHTML = "<h6>Novo Titulo</h6>";
-
-let listaLi = document.querySelectorAll('li');
-
